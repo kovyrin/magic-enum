2c4562ceb327e525930aef295261a12d7f823bbd
Joshua Krall <krall@JoshMac.local>
2008-09-03 16:14:53 -0500

    adding to_sym for string assignment, so that enums can be assigned in params hashes w/ strings

--- lib/magic_enum.rb
+++ lib/magic_enum.rb
@@ -112,6 +112,7 @@ module MagicEnum
       end
   
       define_method "#{name}=" do |value|
+        value = value.to_sym if value.is_a?(String)
         raise ArgumentError, "Invalid value \"#{value}\" for :#{name} attribute of the #{self.class} model" if opts[:raise_on_invalid] and self.class.const_get(enum)[value].nil?
         if value.is_a?(Integer)
           self[name] = value
